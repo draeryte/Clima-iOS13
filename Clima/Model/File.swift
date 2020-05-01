c569e7f647d15b8e7305af7b047630345252569d
Andrae J Henry <andrae.henry@icloud.com>
2020-05-01 09:05:37 -0400

    Code Update

---
+++ Clima/Model/File.swift
@@ -0,0 +1,11 @@
+//
+//  File.swift
+//  Clima
+//
+//  Created by Andrae Henry on 5/1/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+let apiKey = "f39b005b8681ebef4f603e337bd04425"
